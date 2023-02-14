101ca1e1434bc9aea13d8da8a97b9af2ac763c2e
Vicente Olivares <vicente.olivares@exos.mx>
2023-02-13 22:40:08 -0600

    Limpieza de codigo habia quitado una variabl, se regresa en este commit

--- store/index.js
+++ store/index.js
@@ -16,6 +16,7 @@ export const actions = {
   //Note: peticion que traera la consulta que llena la tabla
   async getTaskData({ commit }, payload) {
     let res = await getTask(payload);
+    commit("settask", res.data);
   },
   //Note: traera registro uno a uno para llenar inf de detalle
   async getTaskId({ commit }, payload) {
